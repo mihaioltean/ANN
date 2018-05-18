a1391a8139bdad4d7c506fdd153a69267e10a579
mihaioltean <mihai.oltean@gmail.com>
2018-05-18 16:28:09 +0300

    updated

--- src/ann.cpp
+++ src/ann.cpp
@@ -15,6 +15,8 @@ t_ann::t_ann()
 	out = NULL;
 
 	num_iterations = 10000;
+	deltas = NULL;
+	epoch = 0;
 }
 //------------------------------------------------------
 t_ann::~t_ann()
@@ -37,6 +39,11 @@ void t_ann::allocate_memory(void)
 		for (int neuron = 0; neuron < num_neurons[layer + 1]; neuron++)
 			weights[layer][neuron] = new double[num_neurons[layer] + 1];
 	}
+
+	// allocate deltas
+	deltas = new double*[num_layers - 1];
+	for (int layer = 0; layer < num_layers - 1; layer++)
+		deltas[layer] = new double[num_neurons[layer + 1]];
 }
 //------------------------------------------------------
 void t_ann::release_memory(void)
@@ -53,6 +60,18 @@ void t_ann::release_memory(void)
 		delete[] weights[layer];
 	}
 	delete[] weights;
+
+	// deltas
+	if (deltas) {
+		for (int i = 0; i < num_layers - 1; i++)
+			delete[] deltas[i];
+		delete[] deltas;
+	}
+
+	if (num_neurons) {
+		delete[] num_neurons;
+		num_neurons = NULL;
+	}
 
 }
 //------------------------------------------------------
@@ -85,7 +104,7 @@ int t_ann::get_num_neurons(int layer_index)
 //------------------------------------------------------
 double t_ann::get_weight(int layer_index, int neuron_index, int weight_index)
 {
-
+	return 0; /// must be implemented
 }
 //------------------------------------------------------
 int t_ann::get_num_iterations(void)
@@ -100,9 +119,13 @@ void t_ann::init_weights(void)
 			for (int w = 0; w < num_neurons[layer] + 1; w++)
 				weights[layer][neuron][w] = rand() / (double)RAND_MAX - 0.5;
 	}
-
 }
 //------------------------------------------------------
+int t_ann::get_epoch(void)
+{
+	return epoch;
+}
+//-------------------------------------------------------
 double t_ann::get_error(void)
 {
 	return ann_error;
@@ -111,6 +134,11 @@ double t_ann::get_error(void)
 double logistic_function(double x)
 {
 	return 1 / (1 + exp(-x));
+}
+//------------------------------------------------------
+void t_ann::set_learning_rate(double new_learning_rate)
+{
+	learning_rate = new_learning_rate;
 }
 //------------------------------------------------------
 void t_ann::compute_error(double **training_data, double **target, int num_data)
@@ -135,15 +163,19 @@ void t_ann::compute_error(double **training_data, double **target, int num_data)
 	}
 }
 //------------------------------------------------------
-void t_ann::train(double ** training_data, double **target, int num_data)
+void t_ann::train(double ** training_data, double **target, int num_data, t_func f)
 {
 	allocate_memory();
 	init_weights();
 
-	compute_error(training_data, target, num_data);
+	// first set the value of bias nodes to 1
 
-	for (int epoch = 0; epoch < num_iterations; epoch++) {
-		// printf eroare
+	for (int layer = 0; layer < num_layers - 1; layer++)
+		out[layer][num_neurons[layer]] = 1;
+
+	for (epoch = 0; epoch < num_iterations; epoch++) {
+		compute_error(training_data, target, num_data);
+		f();
 
 		for (int data_index = 0; data_index < num_data; data_index++) {
 			// forward pass
@@ -161,10 +193,26 @@ void t_ann::train(double ** training_data, double **target, int num_data)
 			}
 			// backward pass
 			// update weights between last layer and hidden layer
-			for (int n2 = 0; n2 < num_neurons[num_layers - 1]; n2++)
-				for (int n1 = 0; n1 < num_neurons[num_layers - 1] + 1; n1++)
-					weights[num_layers - 2][n2][n1] -= learning_rate * (out[num_layers - 1][n2] - target[data_index][n2]) * out[num_layers - 1][n2] * (1 - out[num_layers - 1][n2]) * out[num_layers - 2][n1];
+			for (int n2 = 0; n2 < num_neurons[num_layers - 1]; n2++) { // last layer
+				deltas[num_layers - 2][n2] = (out[num_layers - 1][n2] - target[data_index][n2]) * out[num_layers - 1][n2] * (1 - out[num_layers - 1][n2]);
+				for (int neuron_hidden_layer = 0; neuron_hidden_layer < num_neurons[num_layers - 2] + 1; neuron_hidden_layer++) // hidden layer
+					weights[num_layers - 2][n2][neuron_hidden_layer] -= learning_rate * deltas[num_layers - 2][n2] * out[num_layers - 2][neuron_hidden_layer];
+			}
 
+			// weights between input layer and hidden layer
+
+			for (int second_layer_index = num_layers - 2; second_layer_index > 0; second_layer_index--) {
+				for (int neuron_2nd_layer = 0; neuron_2nd_layer < num_neurons[second_layer_index]; neuron_2nd_layer++) {
+					deltas[second_layer_index - 1][neuron_2nd_layer] = 0;
+					double cst = out[second_layer_index][neuron_2nd_layer] * (1 - out[second_layer_index][neuron_2nd_layer]);
+					for (int neuron_3rd_layer = 0; neuron_3rd_layer < num_neurons[second_layer_index + 1]; neuron_3rd_layer++)
+						deltas[second_layer_index - 1][neuron_2nd_layer] += deltas[second_layer_index][neuron_3rd_layer] * weights[second_layer_index][neuron_3rd_layer][neuron_2nd_layer] * cst;
+					for (int neuron_1st_layer = 0; neuron_1st_layer < num_neurons[second_layer_index - 1]; neuron_1st_layer++)
+						weights[second_layer_index - 1][neuron_2nd_layer][neuron_1st_layer] -= learning_rate * deltas[second_layer_index - 1][neuron_2nd_layer] * out[second_layer_index - 1][neuron_1st_layer];
+				}
+			}
+
+		}
 	}
 }
 //------------------------------------------------------
--- src/ann.h
+++ src/ann.h
@@ -1,18 +1,25 @@
 #ifndef ANN_H
 #define ANN_H
 
+typedef void(*t_func)(void);
+
+
 class t_ann {
 private:
 	int num_layers; 
 	int *num_neurons;  // num neurons on each layer
 	double ***weights; // weights[0][0][0] = weight of connection between the first neuron on the first layer
 						// and the first neuron on the second layer
+
+	double **deltas;
 	double **out;
 
 	int num_iterations;
 
 	double ann_error;
 	double learning_rate;
+
+	int epoch;
 
 public:
 
@@ -21,13 +28,14 @@ public:
 	void set_num_layers(int num_layers);
 	void set_num_neurons(int layer_index, int num_neurons);
 	void set_num_iterations(int num_iterations);
+	void set_learning_rate(double new_learning_rate);
 
 	int get_num_layers(void);
 	int get_num_neurons(int layer_index);
 	double get_weight(int layer_index, int neuron_index, int weight_index);
 	int get_num_iterations(void);
 
-	void train(double ** training_data, double **target, int num_data);
+	void train(double ** training_data, double **target, int num_data, t_func);
 
 	void allocate_memory(void);
 	void release_memory(void);
@@ -35,6 +43,8 @@ public:
 
 	double get_error(void);
 	void compute_error(double **training_data, double **target, int num_data);
+
+	int get_epoch(void);
 };
 
 #endif
