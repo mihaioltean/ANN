6324e229ab056aaf0ae5ee5b60c494d1c4af1dca
Mihai Oltean <mihai.oltean@gmail.com>
2018-06-26 11:35:10 +0300

    show error for all data

--- src/test_ann.cpp
+++ src/test_ann.cpp
@@ -9,7 +9,7 @@ t_ann ann;
 
 //---------------------------------------------------------------------------
 
-int _main()
+int main()
 {
 	double **test_data;
 	double **target;
@@ -33,15 +33,27 @@ int _main()
 		return 2;
 	}
 
+	//ann.to_file("ann2.txt");
+
 	int class_index;
 
 	double *out = new double[ann.get_num_neurons(ann.get_num_layers() - 1)];
 
+	int num_incorrectly_classified = 0;
+
 	for (int i = 0; i < num_data; i++) {
 		ann.test(test_data[i], out, class_index);
 		printf("%d\n", class_index);
+
+		if (target[i][class_index] != 1) // we have value 1 on that position if it belongs to class_index
+			num_incorrectly_classified++;
 	}
 
+	printf("num_incorrectly_classified = %d\n", num_incorrectly_classified);
+
+	ann.compute_num_incorrectly_classified(test_data, target, num_data);
+
+	printf("num_incorrectly_classified = %d\n", ann.get_num_incorrectly_classified());
 
 	delete[] out;
 	delete_data(test_data, target, num_data);
